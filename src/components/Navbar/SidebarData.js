35e5bc3cd436a47568f8e47695d9b494b7af7fa3
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-05-18 17:06:39 +0100

    removes extra nav-menu items

--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -23,22 +23,22 @@ export const SidebarData = [
     icon: <FaIcons.FaEdit />,
     cName: 'nav-text',
   },
-  {
-    title: 'Team',
-    path: '/team',
-    icon: <IoIcons.IoMdPeople />,
-    cName: 'nav-text',
-  },
-  {
-    title: 'Messages',
-    path: '/messages',
-    icon: <FaIcons.FaEnvelopeOpenText />,
-    cName: 'nav-text',
-  },
-  {
-    title: 'Support',
-    path: '/support',
-    icon: <IoIcons.IoMdHelpCircle />,
-    cName: 'nav-text',
-  },
+  // {
+  //   title: 'Team',
+  //   path: '/team',
+  //   icon: <IoIcons.IoMdPeople />,
+  //   cName: 'nav-text',
+  // },
+  // {
+  //   title: 'Messages',
+  //   path: '/messages',
+  //   icon: <FaIcons.FaEnvelopeOpenText />,
+  //   cName: 'nav-text',
+  // },
+  // {
+  //   title: 'Support',
+  //   path: '/support',
+  //   icon: <IoIcons.IoMdHelpCircle />,
+  //   cName: 'nav-text',
+  // },
 ];
