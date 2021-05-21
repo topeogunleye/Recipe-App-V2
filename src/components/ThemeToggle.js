8ca595bab6c707d3b9a71cf41261adba0fb71142
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-05-21 07:29:10 +0100

    Adds aria label for theme toggle

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -6,14 +6,17 @@ const ThemeToggle = () => {
   const { toggleTheme } = useContext(ThemeContext);
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
+  const themeAria = isLightTheme
+    ? 'Click to Enable Dark Mode'
+    : 'Click to Enable Light Mode';
 
   return (
     <button
       onClick={toggleTheme}
       className="focus:outline-none my-auto rounded-full w-10 h-10 grid place-items-center ml-2.5"
       style={{ background: theme.bg, color: theme.syntax }}
-      title="Click to Change Theme"
-      aria-label="Close to Change Theme"
+      title={themeAria}
+      aria-label={themeAria}
     >
       <MoonIcon className="h-5 w-5" />
     </button>
