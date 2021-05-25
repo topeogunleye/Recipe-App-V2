2e857dafabc62d14822bd3252d0a27bcd8bad283
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-05-25 21:09:56 +0100

    makes skeleton element ing-title better for mobile devices

--- src/skeletons/SkeletonMealInfo.js
+++ src/skeletons/SkeletonMealInfo.js
@@ -21,8 +21,9 @@ const SkeletonMealInfo = ({ theme }) => {
             <SkeletonElement type="Calories" />
           </div>
           <div className="text-center">
-            <SkeletonElement type="Ing-title" />
-
+            <div className="mx-16 sm:mx-0">
+              <SkeletonElement type="Ing-title" />
+            </div>
             <div className="mt-0 text-center sm:text-justify m-8 single-meal-ul-skel grid grid-cols-4 sm:w-96 place-items-center w-8/12 mx-auto">
               <SkeletonElement type="li-el" className="mx-px" />
               <SkeletonElement type="li-el" className="mx-px" />
