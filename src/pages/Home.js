5663f88b701a7548b9151e80469466dbb900982e
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-14 12:19:49 +0100

    Adds pl-32 to Home page container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -79,7 +79,7 @@ const Home = () => {
         style={{ background: ui, color: syntax }}
       >
         <Navbar />
-        <div className="m-auto lg:pl-24 max-w-md sm:max-w-lg md:max-w-5xl flex flex-col items-center justify-center text-center mb-8">
+        <div className="m-auto lg:pl-32 max-w-md sm:max-w-lg md:max-w-5xl flex flex-col items-center justify-center text-center mb-8">
           <div
             className="absolute top-5 right-10
           "
