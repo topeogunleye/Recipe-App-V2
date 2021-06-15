f024581ce50aceb9aa561705eba3e65f1dd6e499
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-15 13:48:34 +0100

    Removes auuthMethod js and firebase config js

--- src/config/authMethods.js
+++
@@ -1,8 +0,0 @@
-import firebase from "./firebase-config"
-export const facebookProvider = new firebase.auth.FacebookAuthProvider()
-export const githubProvider = new firebase.auth.GithubAuthProvider()
-export const googleProvider = new firebase.auth.GoogleAuthProvider()
-
-
-
-
