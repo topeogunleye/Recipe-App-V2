ce99c24286b8616b47e91e299145586dd1362e84
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-15 16:50:32 +0100

    Adds Firebase js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import "firebase/auth"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+});
+
+export const auth = app.auth()
+export default app
