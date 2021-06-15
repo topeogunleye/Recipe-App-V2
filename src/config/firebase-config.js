d1653111514d3c0ec292e0383c453361e0fc0b50
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-15 08:32:34 +0100

    Adds Firebase Config files

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDiczvKojNODjZWkcmHIk5nSCyP6nWVZN8",
+  authDomain: "fir-auth-dbcb9.firebaseapp.com",
+  databaseURL: "https://fir-auth-dbcb9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-auth-dbcb9",
+  storageBucket: "fir-auth-dbcb9.appspot.com",
+  messagingSenderId: "282043882078",
+  appId: "1:282043882078:web:a4e41cbc742d7c260172cf",
+  measurementId: "G-MGM1995Z12"
+};
