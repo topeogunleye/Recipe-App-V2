07498167d28212f18945cbbfd58d3f605147f647
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-24 12:59:48 +0100

    Imports Dark Theme Toggle to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,37 +12,51 @@ import MealItem from '../components/meal/Meal';
 import logo from '../logo.png';
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
+import DarkToggle from '../components/theme-toggle/DarkToggle';
 
 const Home = () => {
   const [data, setData] = useState({ meals: [] });
   const [query, setQuery] = useState('');
 
   const [url, setUrl] = useState(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`,
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
   );
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+
+
+// Fetch Data with Native JavaScript Fetch
+  // const [data, setData] = useState(null);
+
+  // let fetchData = React.useCallback(async () => {
+  //   const result = await fetch(`api/data/get`);
+  //   const body = await result.json();
+  //   setData(body);
+  //   console.log(data)
+  // },[data])
+
+  // useEffect(() => {
+  //   fetchData();
+  // },[fetchData]);
 
   useEffect(() => {
     const fetchMeal = async () => {
       setIsError(false);
       setIsLoading(true);
- 
+
       try {
-      const result = await axios(url);
- 
-      setData(result.data);
-    } catch (error) {
-      setIsError(true);
-    }
+        const result = await axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
 
       setIsLoading(false);
     };
 
     fetchMeal();
-
-  }, [url] );
-
+  }, [url]);
 
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
@@ -50,21 +64,20 @@ const Home = () => {
   const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(10);
   const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
 
-
   // Get current posts
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts =
     data.meals && data.meals.slice(indexOfFirstPost, indexOfLastPost);
 
-    // Refresh page
-    // Empty will Fetch the default results on the Homepage
-    const empty = ''
-    const refresh = () => {
-      setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${empty}`);
+  // Refresh page
+  // Empty will Fetch the default results on the Homepage
+  const empty = '';
+  const refresh = () => {
+    setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${empty}`);
 
-      setCurrentPage(1)
-    }
+    setCurrentPage(1);
+  };
 
   // Change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
@@ -96,11 +109,9 @@ const Home = () => {
   };
 
   const handleSubmit = (event) => {
-
     setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
 
-    
-    setCurrentPage(1)
+    setCurrentPage(1);
 
     event.preventDefault();
   };
@@ -115,7 +126,7 @@ const Home = () => {
         className="bg-gray-500 text-white min-h-screen"
         style={{ background: ui, color: syntax }}
       >
-        <Navbar refresh={refresh}/>
+        <Navbar refresh={refresh} />
         <div className="m-auto lg:pl-32 max-w-md sm:max-w-lg md:max-w-5xl flex flex-col items-center justify-center text-center mb-8">
           <div
             className="absolute top-5 right-10
@@ -126,6 +137,7 @@ const Home = () => {
               id="random"
             />
           </div>
+         
 
           <SearchBox
             query={query}
@@ -143,7 +155,9 @@ const Home = () => {
           ) : (
             <div id="meals" className="meals">
               {currentPosts &&
-                currentPosts.map((meal) => <MealItem meal={meal} key={uuidv4()} />)}
+                currentPosts.map((meal) => (
+                  <MealItem meal={meal} key={uuidv4()} />
+                ))}
             </div>
           )}
         </div>
