b7d86cd41d38ddd619d65c95ffe1d45bc7aa6407
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-25 12:22:28 +0100

    Imports PrivacyNotice, auth, and Terms of use into App js

--- src/App.js
+++ src/App.js
@@ -10,42 +10,62 @@ import { DarkModeProvider } from './contexts/DarkModeProvider';
 import AuthContextProvider from './contexts/AuthContext';
 import { BookmarkProvider } from './contexts/BookmarkContext';
 import BookMarkView from './pages/bookmark/bookmarkView';
+import PrivacyNotice from './pages/privacynotice/PrivacyNotice'
+import { auth } from './firebase/firebase.utils';
+import { useState, useEffect } from 'react';
+import TermsofUse from './pages/termsofuse/TermsofUse';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+
+      console.log(user);
+    });
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <DarkModeProvider>
           <AuthContextProvider>
-          <BookmarkProvider>
-            <div className="content">
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route exact path="/MealInfo/:mealID">
-                  <MealInfo />
-                </Route>
-                <Route exact path="/Categories">
-                  <Categories />
-                </Route>
-                <Route exact path="/CategoryInfo/:strCategory">
-                  <CategoryInfo />
-                </Route>
-                <Route exact path="/RandomMeal/">
-                  <RandomMeal />
-                </Route>
-                <Route exact path="/NewMealForm/">
-                  <NewMealForm />
-                </Route>
-                <Route exact path="/loginsignup/">
-                  <Loginsignup />
-                </Route>
-                <Route exact path="/Bookmarks/">
-                  <BookMarkView/>
-                </Route>
-              </Switch>
-            </div>
+            <BookmarkProvider>
+              <div className="content">
+                <Switch>
+                  <Route exact path="/">
+                    <Home />
+                  </Route>
+                  <Route exact path="/MealInfo/:mealID">
+                    <MealInfo />
+                  </Route>
+                  <Route exact path="/Categories">
+                    <Categories />
+                  </Route>
+                  <Route exact path="/CategoryInfo/:strCategory">
+                    <CategoryInfo />
+                  </Route>
+                  <Route exact path="/RandomMeal/">
+                    <RandomMeal />
+                  </Route>
+                  <Route exact path="/NewMealForm/">
+                    <NewMealForm />
+                  </Route>
+                  <Route exact path="/loginsignup/">
+                    <Loginsignup />
+                  </Route>
+                  <Route exact path="/privacy/">
+                    <PrivacyNotice />
+                  </Route>
+                  <Route exact path="/terms/">
+                    <TermsofUse/>
+                  </Route>
+                  <Route exact path="/Bookmarks/">
+                    <BookMarkView />
+                  </Route>
+                </Switch>
+              </div>
             </BookmarkProvider>
           </AuthContextProvider>
         </DarkModeProvider>
