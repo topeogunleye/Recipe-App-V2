86d8ad73856c6e6076078073faebd3f00c1ce915
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-27 16:41:24 +0100

    removes variables displayName, email, photoURL, emailVerified, user.uid

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -66,20 +66,6 @@ function Navbar({ refresh }) {
 
   const user = auth.currentUser;
 
-  if (user !== null) {
-    // The user object has basic properties such as display name, email, etc.
-    const displayName = user.displayName;
-    const email = user.email;
-    const photoURL = user.photoURL;
-    const emailVerified = user.emailVerified;
-  
-    // The user's ID, unique to the Firebase project. Do NOT use
-    // this value to authenticate with your backend server, if
-    // you have one. Use User.getToken() instead.
-    const uid = user.uid;
-  }
-
-
   return (
     <>
       <IconContext.Provider value={{ color: syntax }}>
