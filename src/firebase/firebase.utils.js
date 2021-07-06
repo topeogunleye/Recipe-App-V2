1cef01e7e9a1389c7ea90263524affd82fc147e2
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-07-06 23:46:02 +0100

    changes config for firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,47 +1,44 @@
-import firebase from "firebase/app";
-import 'firebase/firestore'
-import 'firebase/auth'
-
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDiczvKojNODjZWkcmHIk5nSCyP6nWVZN8",
-  authDomain: "fir-auth-dbcb9.firebaseapp.com",
-  databaseURL: "https://fir-auth-dbcb9-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fir-auth-dbcb9",
-  storageBucket: "fir-auth-dbcb9.appspot.com",
-  messagingSenderId: "282043882078",
-  appId: "1:282043882078:web:a4e41cbc742d7c260172cf",
-  measurementId: "G-MGM1995Z12"
+  apiKey: 'AIzaSyDiczvKojNODjZWkcmHIk5nSCyP6nWVZN8',
+  authDomain: 'fir-auth-dbcb9.firebaseapp.com',
+  databaseURL:
+    'https://fir-auth-dbcb9-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'fir-auth-dbcb9',
+  storageBucket: 'fir-auth-dbcb9.appspot.com',
+  messagingSenderId: '282043882078',
+  appId: '1:282043882078:web:a4e41cbc742d7c260172cf',
+  measurementId: 'G-MGM1995Z12',
 };
 
 export const createUserProfileDocument = async (userAuth, additonalData) => {
-  if(!userAuth) return;
-  
-  console.log(firestore.doc('users/128hhttfn'))
+  if (!userAuth) return;
 
-}
+  console.log(firestore.doc('users/128hhttfn'));
+};
 
-firebase.initializeApp(config)
-
+firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
-googleProvider.setCustomParameters({propmpt: 'select_account'})
-export const signInWithGoogle = () => auth.signInWithPopup(googleProvider)
+googleProvider.setCustomParameters({ propmpt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 const facebookProvider = new firebase.auth.FacebookAuthProvider();
-facebookProvider.setCustomParameters({propmpt: 'select_account'})
-export const signInWithFacebook = () => auth.signInWithPopup(facebookProvider)
+facebookProvider.setCustomParameters({ propmpt: 'select_account' });
+export const signInWithFacebook = () => auth.signInWithPopup(facebookProvider);
 
 const githubProvider = new firebase.auth.GithubAuthProvider();
-githubProvider.setCustomParameters({propmpt: 'select_account'})
-export const signInWithGithub = () => auth.signInWithPopup(githubProvider)
+githubProvider.setCustomParameters({ propmpt: 'select_account' });
+export const signInWithGithub = () => auth.signInWithPopup(githubProvider);
 
 const twitterProvider = new firebase.auth.TwitterAuthProvider();
-twitterProvider.setCustomParameters({propmpt: 'select_account'})
-export const signInWithTwitter = () => auth.signInWithPopup(twitterProvider)
+twitterProvider.setCustomParameters({ propmpt: 'select_account' });
+export const signInWithTwitter = () => auth.signInWithPopup(twitterProvider);
 
 export default firebase;
