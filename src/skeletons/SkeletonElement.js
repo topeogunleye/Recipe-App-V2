7606aa033629e24e98475fe2140f755766f95362
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-02 13:13:17 +0100

    refactor: Add PropTypes to SkeletonElement component

--- src/skeletons/SkeletonElement.js
+++ src/skeletons/SkeletonElement.js
@@ -1,11 +1,20 @@
-import './Skeleton.css'
+import PropTypes from 'prop-types';
+import './Skeleton.css';
 
 const SkeletonElement = ({ type }) => {
   const classes = `skeleton ${type}`;
 
   return (
-    <div className={classes}></div>
-  )
-}
+    <div className={classes} />
+  );
+};
+
+SkeletonElement.propTypes = {
+  type: PropTypes.string,
+};
+
+SkeletonElement.defaultProps = {
+  type: '',
+};
 
 export default SkeletonElement;
