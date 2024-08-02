3085401a422507bffdc2467cb00cf98b8da2cfa0
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-02 13:11:48 +0100

    chore: Refactor ThemeContextProvider component to use PropTypes for type checking

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,26 +1,40 @@
 import React, { Component, createContext } from 'react';
+import PropTypes from 'prop-types';
 
 export const ThemeContext = createContext();
 
 class ThemeContextProvider extends Component {
-  state = {
-    isLightTheme: true,
-    light: { syntax: '#374151', ui: ' #fff', bg: ' #d1d5db', icon: '#374151' },
-    dark: { syntax: '#fff', ui: ' #374151', bg: ' #111827', icon: '#111827' },
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLightTheme: true,
+      light: {
+        syntax: '#374151', ui: ' #fff', bg: ' #d1d5db', icon: '#374151',
+      },
+      dark: {
+        syntax: '#fff', ui: ' #374151', bg: ' #111827', icon: '#111827',
+      },
+    };
+  }
+
   toggleTheme = () => {
-    this.setState({ isLightTheme: !this.state.isLightTheme });
+    this.setState((prevState) => ({ isLightTheme: !prevState.isLightTheme }));
   };
 
   render() {
+    const { children } = this.props;
     return (
       <ThemeContext.Provider
         value={{ ...this.state, toggleTheme: this.toggleTheme }}
       >
-        {this.props.children}
+        {children}
       </ThemeContext.Provider>
     );
   }
 }
+
+ThemeContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default ThemeContextProvider;
