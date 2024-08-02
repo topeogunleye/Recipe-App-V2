9d726865b67aef714d9ffa130b136b62c5760725
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-02 13:11:33 +0100

    chore: Refactor BookmarkProvider component to use PropTypes for type checking

--- src/contexts/BookmarkContext.js
+++ src/contexts/BookmarkContext.js
@@ -1,15 +1,24 @@
+import PropTypes from 'prop-types';
 import { useState, createContext } from 'react';
 
 const BookmarkContext = createContext({});
 
-const BookmarkProvider = (props) => {
-  let [bookmarks, setBookmarks] = useState([]);
+const BookmarkProvider = ({ children }) => {
+  const [bookmarks, setBookmarks] = useState([]);
 
   return (
-    <BookmarkContext.Provider value={{bookmarks, setBookmarks}}>
-      {props.children}
+    <BookmarkContext.Provider value={{ bookmarks, setBookmarks }}>
+      {children}
     </BookmarkContext.Provider>
   );
+};
+
+BookmarkProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+BookmarkProvider.defaultProps = {
+  children: null,
 };
 
 export { BookmarkContext, BookmarkProvider };
