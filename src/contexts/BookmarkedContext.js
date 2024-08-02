72ed08a5408cc73734e04fe7c73c0f3625d73804
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-02 13:11:38 +0100

    chore: Refactor BookmarkedProvider component to use PropTypes for type checking

--- src/contexts/BookmarkedContext.js
+++ src/contexts/BookmarkedContext.js
@@ -1,27 +1,31 @@
+import PropTypes from 'prop-types';
 import { useState, createContext, useEffect } from 'react';
 import useFetchMealDbApi from '../components/useFetchMealDbApi';
 
 const BookmarkedContext = createContext({});
 
-const BookmarkedProvider = (props) => {
+const BookmarkedProvider = ({ children }) => {
   const [storedBookmarked, setStoredBookmarked] = useState([]);
 
   setStoredBookmarked(JSON.parse(localStorage.getItem('bookmarks')));
 
-  const [{ data }, doFetch] = useFetchMealDbApi()
-useEffect(
-  () =>
-    doFetch(`https://www.themealdb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/lookup.php?i=${storedBookmarked.idMeal}`),
-  [doFetch, data, storedBookmarked]
-);
+  const [{ data }, doFetch] = useFetchMealDbApi();
+  useEffect(
+    () => doFetch(`https://www.themealdb.com/api/json/v2/${process.env.REACT_APP_API_KEY}/lookup.php?i=${storedBookmarked.idMeal}`),
+    [doFetch, data, storedBookmarked],
+  );
 
-  let [bookmarked, setBookmarked] = useState([]);
+  const [bookmarked, setBookmarked] = useState([]);
 
   return (
-    <BookmarkedContext.Provider value={{bookmarked, setBookmarked}}>
-      {props.children}
+    <BookmarkedContext.Provider value={{ bookmarked, setBookmarked }}>
+      {children}
     </BookmarkedContext.Provider>
   );
-}
+};
+
+BookmarkedProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export { BookmarkedContext, BookmarkedProvider };
