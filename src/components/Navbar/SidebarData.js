093a71045b7311dd30645538e35b3484b3ae40c6
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-02 13:09:46 +0100

    chore: Remove commented out code and unused sidebar items

--- src/components/Navbar/SidebarData.js
+++ src/components/Navbar/SidebarData.js
@@ -3,7 +3,7 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as MdIcons from 'react-icons/md';
 
-export const SidebarData = [
+const SidebarData = [
   {
     title: 'Home',
     path: '/',
@@ -28,22 +28,6 @@ export const SidebarData = [
     icon: <FaIcons.FaBookmark />,
     cName: 'nav-text',
   },
-  // {
-  //   title: 'Team',
-  //   path: '/team',
-  //   icon: <IoIcons.IoMdPeople />,
-  //   cName: 'nav-text',
-  // },
-  // {
-  //   title: 'Messages',
-  //   path: '/messages',
-  //   icon: <FaIcons.FaEnvelopeOpenText />,
-  //   cName: 'nav-text',
-  // },
-  // {
-  //   title: 'Support',
-  //   path: '/support',
-  //   icon: <IoIcons.IoMdHelpCircle />,
-  //   cName: 'nav-text',
-  // },
 ];
+
+export default SidebarData;
