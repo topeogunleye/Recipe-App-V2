d2c69e913bf226f18e9080d177374d5ede5d7e13
Temitope Ogunleye <topeogunleye1@gmail.com>
2024-08-02 11:12:20 +0100

    chore: Update firebase.js to use consistent import statements

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app'
-import "firebase/auth"
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const app = firebase.initializeApp({
@@ -10,8 +10,8 @@ const app = firebase.initializeApp({
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
-export const auth = app.auth()
-export default app
+export const auth = app.auth();
+export default app;
